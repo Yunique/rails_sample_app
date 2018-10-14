3eb5ecf056f918d7bcc72bd128e65d232bb4af3d
Yunus Ganiyev <yunique33@gmail.com>
2018-10-15 01:23:42 +0300

    finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Yunus Ganiyev"
+    email    "yunique33@gmail.com"
+    password "17141320"
+    password_confirmation "17141320"
+  end
+end
